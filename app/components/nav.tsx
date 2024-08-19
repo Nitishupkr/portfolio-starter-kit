bf83d1d4c4199f666f6f341aa129fd0885892287
Nitishupkr <nitishupkr@gmail.com>
2024-08-19 14:07:36 +0530

    c

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import Logo from 'app/components/logo' // Adjust the path as needed
 
 const navItems = {
   '/': {
@@ -14,20 +15,27 @@ const navItems = {
 
 export function Navbar() {
   return (
-    <aside className="-ml-[8px] mb-16 tracking-tight text-xl">
-      
-      <div className="lg:sticky lg:top-20">
+    <aside className="mb-16 tracking-tight text-xl">
+      <div className="lg:sticky lg:top-10">
         <nav
-          className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
+          className="flex items-center justify-between px-4 fade md:overflow-auto"
           id="nav"
         >
-          <div className="flex flex-row space-x-0 pr-10">
+          {/* Logo Section */}
+          <div className="flex order-2 md:order-1 md:flex md:flex-row items-center">
+            <Link href="/">
+              <Logo className="transition-all hover:opacity-80 md:mr-8 md:ml-0 ml-2" />
+            </Link>
+          </div>
+
+          {/* Navigation Links */}
+          <div className="flex order-1 md:order-2 space-x-4">
             {Object.entries(navItems).map(([path, { name }]) => {
               return (
                 <Link
                   key={path}
                   href={path}
-                  className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
+                  className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 py-1 px-2"
                 >
                   {name}
                 </Link>
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,30 @@
 import { BlogPosts } from 'app/components/posts'
-import Logo from 'app/components/logo' // Adjust the path as needed
+import Image from 'next/image'
 
 export default function Page() {
   return (
     <section>
       <div className="mb-4">
-        <div className="fixed top-0 left-0 p-4">
-          <Logo /> {/* Use the Logo component here */}
+        {/* Image with Animation */}
+        <div className="animate-fade-in mb-4">
+          <Image
+            src="https://res.cloudinary.com/dwxxuolek/image/upload/v1724056044/Blue_and_White_Minimal_Modern_Simple_Typography_Art_Workshop_Instagram_Story_btei64.png"
+            alt="Art Workshop"
+            width={200} // Adjust width as needed
+            height={100} // Adjust height as needed
+            className="rounded-lg shadow-lg"
+          />
         </div>
       </div>
-      <p className="mb-4 text-xl"> {/* Adjust the font size here */}
+
+      <p className="mb-4 text-xl">
         {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
         Vim's keystroke commands and tabs' flexibility for personal viewing
         preferences. This extends to my support for static typing, where its
         early error detection ensures cleaner code, and my preference for dark
         mode, which eases long coding sessions by reducing eye strain.`}
       </p>
+
       <div className="my-8 text-xl">
         <BlogPosts />
       </div>
